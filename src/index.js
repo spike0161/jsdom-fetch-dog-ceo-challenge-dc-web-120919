7a52802c416b1b11c8d305dd3792bef284851af6
github username <github email address>
2020-01-30 11:43:41 -0500

    Got pictures working

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log('%c HI', 'color: firebrick')
+let dogsImg
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+      .then( response => response.json())
+      .then(data => data.message.forEach(dog => renderDog(dog)))
+
+    })
+
+
+
+
+
+
+
+
+function renderDog(dog) {
+  let dogCard = document.getElementById("dog-image-container")
+  let imageHolder = document.createElement('img')
+  imageHolder.src = dog
+  dogCard.appendChild(imageHolder)
+}
